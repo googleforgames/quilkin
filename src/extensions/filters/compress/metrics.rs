461ed6360b2ca6eccbe8aea6b246f79922ee0fe0
Mark Mandel <markmandel@google.com>
2021-01-28 09:52:37 -0800

    Implementation of Compression Filter. (#162)
    * Implementation of Compression Filter.
    
    Default implementation uses Snappy compression, but it has been
    implemented such that we can add extra compression modes over time.
    
    Next up are documentation and integration tests. After that we will
    tackle an integration test (and corresponding fixes) wherein we run both
    authentication filters and compression to validate correct ordering of
    Filters on the `FilterChain`.

---
+++ src/extensions/filters/compress/metrics.rs
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2020 Google LLC All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+use prometheus::core::{AtomicI64, GenericCounter};
+use prometheus::{IntCounter, Registry};
+use prometheus::{IntCounterVec, Result as MetricsResult};
+
+use crate::metrics::{filter_opts, CollectorExt};
+
+/// Register and manage metrics for this filter
+pub(super) struct Metrics {
+    pub(super) packets_dropped_compress: GenericCounter<AtomicI64>,
+    pub(super) packets_dropped_decompress: GenericCounter<AtomicI64>,
+    pub(super) compressed_bytes_total: GenericCounter<AtomicI64>,
+    pub(super) decompressed_bytes_total: GenericCounter<AtomicI64>,
+}
+
+impl Metrics {
+    pub(super) fn new(registry: &Registry) -> MetricsResult<Self> {
+        let operation_labels = vec!["operation"];
+        let dropped_metric = IntCounterVec::new(
+            filter_opts(
+                "packets_dropped_total",
+                "Compress",
+                "Total number of packets dropped as they could not be processed. Labels: operation.",
+            ),
+            &operation_labels,
+        )?
+        .register(registry)?;
+
+        let decompressed_bytes_total = IntCounter::with_opts(filter_opts(
+            "decompressed_bytes_total",
+            "Compress",
+            "Total number of decompressed bytes either received or sent.",
+        ))?
+        .register(registry)?;
+
+        let compressed_bytes_total = IntCounter::with_opts(filter_opts(
+            "compressed_bytes_total",
+            "Compress",
+            "Total number of compressed bytes either received or sent.",
+        ))?
+        .register(registry)?;
+
+        Ok(Metrics {
+            packets_dropped_compress: dropped_metric
+                .get_metric_with_label_values(vec!["Compress"].as_slice())?,
+            packets_dropped_decompress: dropped_metric
+                .get_metric_with_label_values(vec!["Decompress"].as_slice())?,
+            compressed_bytes_total,
+            decompressed_bytes_total,
+        })
+    }
+}
