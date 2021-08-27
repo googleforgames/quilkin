c168215fc8ae4402ffce3171b04fc09215ff50f1
Giles Heron <gilesheron@users.noreply.github.com>
2021-08-27 10:18:29 +0100

    Merge all commits for hash mode load balancer

--- src/filters/load_balancer/config.rs
+++ src/filters/load_balancer/config.rs
@@ -21,7 +21,7 @@ use std::convert::TryFrom;
 use serde::{Deserialize, Serialize};
 
 use self::quilkin::extensions::filters::load_balancer::v1alpha1::load_balancer::Policy as ProtoPolicy;
-use super::endpoint_chooser::{EndpointChooser, RandomEndpointChooser, RoundRobinEndpointChooser};
+use super::endpoint_chooser::{EndpointChooser, RandomEndpointChooser, RoundRobinEndpointChooser, HashEndpointChooser};
 use crate::{filters::ConvertProtoConfigError, map_proto_enum};
 
 pub use self::quilkin::extensions::filters::load_balancer::v1alpha1::LoadBalancer as ProtoConfig;
@@ -46,7 +46,7 @@ impl TryFrom<ProtoConfig> for Config {
                     field = "policy",
                     proto_enum_type = ProtoPolicy,
                     target_enum_type = Policy,
-                    variants = [RoundRobin, Random]
+                    variants = [RoundRobin, Random, Hash]
                 )
             })
             .transpose()?
@@ -65,6 +65,9 @@ pub enum Policy {
     /// Send packets to endpoints chosen at random.
     #[serde(rename = "RANDOM")]
     Random,
+    /// Send packets to endpoints based on hash of source IP
+    #[serde(rename = "HASH")]
+    Hash,
 }
 
 impl Policy {
@@ -72,6 +75,7 @@ impl Policy {
         match self {
             Policy::RoundRobin => Box::new(RoundRobinEndpointChooser::new()),
             Policy::Random => Box::new(RandomEndpointChooser),
+            Policy::Hash => Box::new(HashEndpointChooser),
         }
     }
 }
@@ -116,6 +120,17 @@ mod tests {
                 },
                 Some(Config {
                     policy: Policy::RoundRobin,
+                }),
+            ),
+            (
+                "HashPolicy",
+                ProtoConfig {
+                    policy: Some(PolicyValue {
+                        value: ProtoPolicy::Hash as i32,
+                    }),
+                },
+                Some(Config {
+                    policy: Policy::Hash,
                 }),
             ),
             (
