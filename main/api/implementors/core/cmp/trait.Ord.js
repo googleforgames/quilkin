b9518ebabe78d8a05d4bc346d64458cab8dd9c70
markmandel <markmandel@google.com>
2021-10-06 16:56:15 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@8cc0e7d3016496ad65b69ee599b3acf24c1cd836 🚀

--- main/api/implementors/core/cmp/trait.Ord.js
+++ main/api/implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["quilkin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"quilkin/endpoint/enum.RetainedItems.html\" title=\"enum quilkin::endpoint::RetainedItems\">RetainedItems</a>","synthetic":false,"types":["quilkin::endpoint::RetainedItems"]}];
+implementors["quilkin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"quilkin/endpoint/enum.RetainedItems.html\" title=\"enum quilkin::endpoint::RetainedItems\">RetainedItems</a>","synthetic":false,"types":["quilkin::endpoint::RetainedItems"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
