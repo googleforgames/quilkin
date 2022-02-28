11b19656c8a38b929ed090de9232350d0c6f808a
markmandel <markmandel@google.com>
2022-02-28 19:07:51 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@5e82d0a5e1a5352a8b7df15ae51670dcdbc6d184 🚀

--- main/api/implementors/core/hash/trait.Hash.js
+++ main/api/implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["quilkin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"quilkin/endpoint/struct.EndpointAddress.html\" title=\"struct quilkin::endpoint::EndpointAddress\">EndpointAddress</a>","synthetic":false,"types":["quilkin::endpoint::address::EndpointAddress"]}];
+implementors["quilkin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"quilkin/endpoint/struct.EndpointAddress.html\" title=\"struct quilkin::endpoint::EndpointAddress\">EndpointAddress</a>","synthetic":false,"types":["quilkin::endpoint::address::EndpointAddress"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
