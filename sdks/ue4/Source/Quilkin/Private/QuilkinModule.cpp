726e5230b57af23402fa65b011ff24e4547fc61e
Rezvaneh Faraji <rezvaneh.faraji@embark-studios.com>
2022-05-05 08:25:25 +0200

    doc and Apache license

--- sdks/ue4/Source/Quilkin/Private/QuilkinModule.cpp
+++ sdks/ue4/Source/Quilkin/Private/QuilkinModule.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "QuilkinModule.h"
 #include "QuilkinLog.h"
 
@@ -21,9 +37,9 @@ void FQuilkinModule::StartupModule()
 		return;
 	}
 
-	FSocketSubsystemModule& SocketSubsystemModule = FModuleManager::LoadModuleChecked<FSocketSubsystemModule>("Sockets");
+	FSocketSubsystemModule &SocketSubsystemModule = FModuleManager::LoadModuleChecked<FSocketSubsystemModule>("Sockets");
 
-	ISocketSubsystem* DefaultSocketSubsystem = SocketSubsystemModule.GetSocketSubsystem();
+	ISocketSubsystem *DefaultSocketSubsystem = SocketSubsystemModule.GetSocketSubsystem();
 	if (DefaultSocketSubsystem == nullptr)
 	{
 		UE_LOG(LogQuilkin, Log, TEXT("No default SocketSubsystem was set. Will not use Quilkin SocketSubsystem"));
@@ -44,7 +60,7 @@ void FQuilkinModule::ShutdownModule()
 		return;
 	}
 
-	FSocketSubsystemModule& SocketSubsystemModule = FModuleManager::LoadModuleChecked<FSocketSubsystemModule>("Sockets");
+	FSocketSubsystemModule &SocketSubsystemModule = FModuleManager::LoadModuleChecked<FSocketSubsystemModule>("Sockets");
 	SocketSubsystemModule.UnregisterSocketSubsystem(QUILKIN_SOCKETSUBSYSTEM_NAME);
 	QuilkinSocketSubsystem.Reset();
 }
