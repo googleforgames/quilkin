888a8708bea08ec941be1fabfce59ff892ba8129
markmandel <markmandel@google.com>
2022-06-06 23:55:13 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@37423cd93cb90c49aa2f88c7bd8e7ae270865902 🚀

--- main/api/implementors/core/ops/deref/trait.Deref.js
+++ main/api/implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["quilkin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"quilkin/endpoint/struct.Endpoints.html\" title=\"struct quilkin::endpoint::Endpoints\">Endpoints</a>","synthetic":false,"types":["quilkin::endpoint::Endpoints"]}];
+implementors["quilkin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"quilkin/endpoint/struct.Endpoints.html\" title=\"struct quilkin::endpoint::Endpoints\">Endpoints</a>","synthetic":false,"types":["quilkin::endpoint::Endpoints"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
