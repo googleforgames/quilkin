8c33864f6342328498518d0203c905244816a26c
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2022-08-22 15:53:49 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@b9cd1f662eae1129ee875101db725492b2df01a9 🚀

---
+++ main/api/implementors/clap/derive/trait.Parser.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["quilkin"] = [{"text":"impl Parser for <a class=\"struct\" href=\"quilkin/struct.Cli.html\" title=\"struct quilkin::Cli\">Cli</a>","synthetic":false,"types":["quilkin::cli::Cli"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
