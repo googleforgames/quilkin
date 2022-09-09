f1a0b5607a6e1d139682938b4ce5e44848d82085
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2022-09-09 08:49:34 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@803809d6ce1d98ba65cc42947c4f26e09583622b 🚀

---
+++ main/api/implementors/kube_core/object/trait.HasStatus.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["quilkin"] = [{"text":"impl HasStatus for <a class=\"struct\" href=\"quilkin/config/watch/agones/crd/struct.Fleet.html\" title=\"struct quilkin::config::watch::agones::crd::Fleet\">Fleet</a>","synthetic":false,"types":["quilkin::config::watch::agones::crd::Fleet"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
