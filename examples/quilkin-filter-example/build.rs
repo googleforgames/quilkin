1087416ca9775ddfbc64755248f5906dc5e1d5e6
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2022-11-30 19:59:52 +0100

    Update dependencies (#651)
    * Update dependencies
    
    * vendor-protoc feature (#653)
    
    Co-authored-by: Mark Mandel <markmandel@google.com>

--- examples/quilkin-filter-example/build.rs
+++ examples/quilkin-filter-example/build.rs
@@ -16,6 +16,9 @@
 
 // ANCHOR: build
 fn main() {
+    // Remove if you already have `protoc` installed in your system.
+    std::env::set_var("PROTOC", protobuf_src::protoc());
+
     prost_build::compile_protos(&["src/greet.proto"], &["src/"]).unwrap();
 }
 // ANCHOR_END: build
