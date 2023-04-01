f7ade4944617d5c8674bcad19fd7a5ee72134864
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2023-04-01 06:59:35 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@22b8ff8df68c695e8a6cb7e9e744f964d5eb6d7a 🚀

--- main/api/quilkin/filters/token_router/sidebar-items.js
+++ main/api/quilkin/filters/token_router/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["Config",""],["TokenRouter","Filter that only allows packets to be passed to Endpoints that have a matching connection_id to the token stored in the Filter’s dynamic metadata."]]};
+window.SIDEBAR_ITEMS = {"enum":[["Error",""]],"struct":[["Config",""],["TokenRouter","Filter that only allows packets to be passed to Endpoints that have a matching connection_id to the token stored in the Filter’s dynamic metadata."]]};
