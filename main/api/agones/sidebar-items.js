81640097d80bcd7c65d610b457c45e3c1c5f4ed6
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2023-07-14 22:51:39 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@0ec3daa8e391ca91dca43bd96ce9fa776bba206f 🚀

--- main/api/agones/sidebar-items.js
+++ main/api/agones/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["GAMESERVER_IMAGE"],"fn":["fleet","game_server","gameserver_address","is_deployment_ready","is_fleet_ready","is_gameserver_ready","is_pod_ready","quilkin_config_map","quilkin_container"],"struct":["Client"]};
+window.SIDEBAR_ITEMS = {"constant":["GAMESERVER_IMAGE"],"fn":["debug_pods","fleet","game_server","gameserver_address","is_deployment_ready","is_fleet_ready","is_gameserver_ready","is_pod_ready","quilkin_config_map","quilkin_container"],"struct":["Client"]};
