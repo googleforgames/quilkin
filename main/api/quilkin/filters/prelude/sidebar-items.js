a110f0051f160c5a370d5eea7e3e09d2a20b9fb3
markmandel <markmandel@google.com>
2023-08-04 17:23:36 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@0ed80ff9ab8dc0f4e976c940b77242ba251e2a7c 🚀

--- main/api/quilkin/filters/prelude/sidebar-items.js
+++ main/api/quilkin/filters/prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CreationError"],"struct":["ConvertProtoConfigError","CreateFilterArgs","FilterError","FilterInstance","ReadContext","WriteContext"]};
+window.SIDEBAR_ITEMS = {};
