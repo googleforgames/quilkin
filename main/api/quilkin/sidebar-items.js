81c3c6d8c355b7ddb0b424031f0ee0bbe2b9ebfa
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2023-11-13 18:37:28 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@044de434d7bd61d40d9d2799309b0e2e325b5ddc 🚀

--- main/api/quilkin/sidebar-items.js
+++ main/api/quilkin/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"macro":["include_proto"],"mod":["cli","codec","config","filters","net"],"struct":["Cli","Config","Proxy"],"type":["Result"]};
+window.SIDEBAR_ITEMS = {"enum":["ShutdownKind"],"fn":["make_shutdown_channel"],"macro":["include_proto"],"mod":["cli","codec","config","filters","net"],"struct":["Cli","Config","Proxy"],"type":["Result","ShutdownRx","ShutdownTx"]};
