6240a960894accc1a4608d50ff5ad28d97fa874d
Jake Shadle <jake.shadle@embark-studios.com>
2023-11-23 11:53:09 +0100

    Fix PoolBuffer::split (#869)
    * Update lockfile
    
    * Fix poolbuffer splitting

--- tests/filter_order.rs
+++ tests/filter_order.rs
@@ -101,3 +101,60 @@ on_write: DECOMPRESS
 
     assert_eq!(&buf, hellos.as_bytes(),);
 }
+
+#[tokio::test]
+async fn multiple_mutations() {
+    let filters = r#"
+- name: quilkin.filters.capture.v1alpha1.Capture
+  config:
+    metadataKey: embark.dev/load_balancer/version
+    suffix:
+      size: 1
+      remove: true
+- name: quilkin.filters.capture.v1alpha1.Capture
+  config:
+    metadataKey: embark.dev/load_balancer/token
+    suffix:
+      size: 16
+      remove: true
+"#;
+
+    let filters: Vec<Filter> = serde_yaml::from_str(filters).unwrap();
+
+    let mut t = TestHelper::default();
+    let mut echo = t
+        .run_echo_server_with_tap(&AddressType::Random, move |_, bytes, _| {
+            assert_eq!(b"hello", bytes);
+        })
+        .await;
+
+    quilkin::test::map_to_localhost(&mut echo).await;
+    let server_config = std::sync::Arc::new(quilkin::Config::default());
+    server_config
+        .clusters
+        .modify(|clusters| clusters.insert_default([Endpoint::new(echo.clone())].into()));
+    server_config.filters.store(
+        quilkin::filters::FilterChain::try_from(filters)
+            .map(std::sync::Arc::new)
+            .unwrap(),
+    );
+
+    let server_port = t.run_server(server_config, None, None).await;
+
+    // let's send the packet
+    let (mut recv_chan, socket) = t.open_socket_and_recv_multiple_packets().await;
+
+    let local_addr = (Ipv4Addr::LOCALHOST, server_port);
+
+    socket
+        .send_to(b"helloxxxxxxxxxxxxxxxx6", &local_addr)
+        .await
+        .unwrap();
+
+    let received = timeout(Duration::from_millis(500), recv_chan.recv())
+        .await
+        .expect("should have received a packet")
+        .unwrap();
+
+    assert_eq!(b"hello", received.as_bytes());
+}
