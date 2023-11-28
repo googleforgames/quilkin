7a3035523956692e92e76a0ddd003b2e0009ebd4
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2023-11-28 08:55:27 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@8197bdd23b495d13b1aad816254f096796e91134 🚀

--- main/api/implementors/kube_core/resource/trait.Resource.js
+++ main/api/implementors/kube_core/resource/trait.Resource.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"quilkin":[["impl Resource for <a class=\"struct\" href=\"quilkin/config/providers/k8s/agones/struct.Fleet.html\" title=\"struct quilkin::config::providers::k8s::agones::Fleet\">Fleet</a>"],["impl Resource for <a class=\"struct\" href=\"quilkin/config/providers/k8s/agones/struct.GameServer.html\" title=\"struct quilkin::config::providers::k8s::agones::GameServer\">GameServer</a>"]]
+"quilkin":[["impl Resource for <a class=\"struct\" href=\"quilkin/config/providers/k8s/agones/struct.GameServer.html\" title=\"struct quilkin::config::providers::k8s::agones::GameServer\">GameServer</a>"],["impl Resource for <a class=\"struct\" href=\"quilkin/config/providers/k8s/agones/struct.Fleet.html\" title=\"struct quilkin::config::providers::k8s::agones::Fleet\">Fleet</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
