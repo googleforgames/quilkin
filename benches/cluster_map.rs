1a7d3ab54f6446431d80683579b116f5520e7b9e
Jake Shadle <jake.shadle@embark-studios.com>
2024-02-26 10:34:49 +0100

    ClusterMap benchmarks (#856)
    * Checkpoint
    
    * Add serde benchmarks for ClusterMap
    
    * Improve locality ser/de
    
    * Oops

---
+++ benches/cluster_map.rs
@@ -0,0 +1,360 @@
+use std::{
+    collections::BTreeSet,
+    hash::{Hash as _, Hasher as _},
+    net::{Ipv4Addr, Ipv6Addr},
+};
+
+use divan::Bencher;
+use quilkin::net::{cluster::ClusterMap, endpoint::Locality, Endpoint, EndpointAddress};
+use rand::Rng;
+use xxhash_rust::xxh3::Xxh3 as Hasher;
+
+const LOCALITIES: &[&str] = &[
+    "us:east1:b",
+    "us:east1:c",
+    "us:east1:d",
+    "us:east4:c",
+    "us:east4:b",
+    "us:east4:a",
+    "us:central1:c",
+    "us:central1:a",
+    "us:central1:f",
+    "us:central1:b",
+    "us:west1:b",
+    "us:west1:c",
+    "us:west1:a",
+    "europe:west4:a",
+    "europe:west4:b",
+    "europe:west4:c",
+    "europe:west1:b",
+    "europe:west1:d",
+    "europe:west1:c",
+    "europe:west3:c",
+    "europe:west3:a",
+    "europe:west3:b",
+    "europe:west2:c",
+    "europe:west2:b",
+    "europe:west2:a",
+    "asia:east1:b",
+    "asia:east1:a",
+    "asia:east1:c",
+    "asia:southeast1:b",
+    "asia:southeast1:a",
+    "asia:southeast1:c",
+    "asia:northeast1:b",
+    "asia:northeast1:c",
+    "asia:northeast1:a",
+    "asia:south1:c",
+    "asia:south1:b",
+    "asia:south1:a",
+    "australia:southeast1:b",
+    "australia:southeast1:c",
+    "australia:southeast1:a",
+    "southamerica:east1:b",
+    "southamerica:east1:c",
+    "southamerica:east1:a",
+    "asia:east2:a",
+    "asia:east2:b",
+    "asia:east2:c",
+    "asia:northeast2:a",
+    "asia:northeast2:b",
+    "asia:northeast2:c",
+    "asia:northeast3:a",
+    "asia:northeast3:b",
+    "asia:northeast3:c",
+    "asia:south2:a",
+    "asia:south2:b",
+    "asia:south2:c",
+    "asia:southeast2:a",
+    "asia:southeast2:b",
+    "asia:southeast2:c",
+    "australia:southeast2:a",
+    "australia:southeast2:b",
+    "australia:southeast2:c",
+    "europe:central2:a",
+    "europe:central2:b",
+    "europe:central2:c",
+    "europe:north1:a",
+    "europe:north1:b",
+    "europe:north1:c",
+    "europe:southwest1:a",
+    "europe:southwest1:b",
+    "europe:southwest1:c",
+    "europe:west10:a",
+    "europe:west10:b",
+    "europe:west10:c",
+    "europe:west12:a",
+    "europe:west12:b",
+    "europe:west12:c",
+    "europe:west6:a",
+    "europe:west6:b",
+    "europe:west6:c",
+    "europe:west8:a",
+    "europe:west8:b",
+    "europe:west8:c",
+    "europe:west9:a",
+    "europe:west9:b",
+    "europe:west9:c",
+    "me:central1:a",
+    "me:central1:b",
+    "me:central1:c",
+    "me:central2:a",
+    "me:central2:b",
+    "me:central2:c",
+    "me:west1:a",
+    "me:west1:b",
+    "me:west1:c",
+    "northamerica:northeast1:a",
+    "northamerica:northeast1:b",
+    "northamerica:northeast1:c",
+    "northamerica:northeast2:a",
+    "northamerica:northeast2:b",
+    "northamerica:northeast2:c",
+    "southamerica:west1:a",
+    "southamerica:west1:b",
+    "southamerica:west1:c",
+    "us:east5:a",
+    "us:east5:b",
+    "us:east5:c",
+    "us:south1:a",
+    "us:south1:b",
+    "us:south1:c",
+    "us:west2:a",
+    "us:west2:b",
+    "us:west2:c",
+    "us:west3:a",
+    "us:west3:b",
+    "us:west3:c",
+    "us:west4:a",
+    "us:west4:b",
+    "us:west4:c",
+];
+
+fn gen_endpoints(rng: &mut rand::rngs::SmallRng, hasher: &mut Hasher) -> BTreeSet<Endpoint> {
+    let num_endpoints = rng.gen_range(100..10_000);
+    hasher.write_u16(num_endpoints);
+
+    let mut endpoints = BTreeSet::new();
+
+    for i in 0..num_endpoints {
+        let ep_addr = match i % 3 {
+            0 => (Ipv4Addr::new(100, 200, (i >> 8) as _, (i & 0xff) as _), i).into(),
+            1 => EndpointAddress {
+                host: quilkin::net::endpoint::AddressKind::Name(format!("benchmark-{i}")),
+                port: i,
+            },
+            2 => (Ipv6Addr::new(100, 200, i, 0, 0, 1, 2, 3), i).into(),
+            _ => unreachable!(),
+        };
+
+        endpoints.insert(Endpoint::new(ep_addr));
+    }
+
+    for ep in &endpoints {
+        ep.address.hash(hasher);
+    }
+
+    endpoints
+}
+
+#[allow(dead_code)]
+struct GenCluster {
+    cm: ClusterMap,
+    hash: u64,
+    total_endpoints: usize,
+    sets: std::collections::BTreeMap<Option<Locality>, BTreeSet<Endpoint>>,
+}
+
+#[inline]
+fn write_locality(hasher: &mut Hasher, loc: &Option<Locality>) {
+    if let Some(key) = loc {
+        key.hash(hasher);
+    } else {
+        hasher.write("None".as_bytes());
+    }
+}
+
+fn gen_cluster_map<const S: u64>() -> GenCluster {
+    use rand::prelude::*;
+
+    let mut rng = rand::rngs::SmallRng::seed_from_u64(S);
+
+    let mut hasher = Hasher::with_seed(S);
+    let mut total_endpoints = 0;
+
+    let num_locals = rng.gen_range(10..LOCALITIES.len());
+
+    // Select how many localities we want, note we add 1 since we always have a default cluster
+    hasher.write_usize(num_locals + 1);
+
+    let cm = ClusterMap::default();
+
+    for locality in LOCALITIES.choose_multiple(&mut rng, num_locals) {
+        let locality = locality.parse().unwrap();
+        cm.insert(Some(locality), Default::default());
+    }
+
+    // Now actually insert the endpoints, now that the order of keys is established,
+    // annoying, but note we split out iteration versus insertion, otherwise we deadlock
+    let keys: Vec<_> = cm.iter().map(|kv| kv.key().clone()).collect();
+    let mut sets = std::collections::BTreeMap::new();
+
+    for key in keys {
+        write_locality(&mut hasher, &key);
+
+        let ep = gen_endpoints(&mut rng, &mut hasher);
+        total_endpoints += ep.len();
+        cm.insert(key.clone(), ep.clone());
+        sets.insert(key, ep);
+    }
+
+    GenCluster {
+        cm,
+        hash: hasher.finish(),
+        total_endpoints,
+        sets,
+    }
+}
+
+#[divan::bench_group(sample_count = 10)]
+mod serde {
+    use super::*;
+    use prost_types::Any;
+    use quilkin::net::cluster::proto::Cluster;
+
+    fn serialize_to_protobuf(cm: &ClusterMap) -> Vec<Any> {
+        let mut resources = Vec::new();
+        let resource_type = quilkin::net::xds::ResourceType::Cluster;
+
+        for cluster in cm.iter() {
+            resources.push(
+                resource_type
+                    .encode_to_any(&Cluster::try_from((cluster.key(), cluster.value())).unwrap())
+                    .unwrap(),
+            );
+        }
+
+        resources
+    }
+
+    fn deserialize_from_protobuf(pv: Vec<Any>) -> ClusterMap {
+        let cm = ClusterMap::default();
+
+        for any in pv {
+            let c = quilkin::net::xds::Resource::try_from(any).unwrap();
+
+            let quilkin::net::xds::Resource::Cluster(cluster) = c else {
+                unreachable!()
+            };
+            cm.merge(
+                cluster.locality.map(From::from),
+                cluster
+                    .endpoints
+                    .into_iter()
+                    .map(TryFrom::try_from)
+                    .collect::<Result<_, _>>()
+                    .unwrap(),
+            );
+        }
+
+        cm
+    }
+
+    fn serialize_to_json(cm: &ClusterMap) -> serde_json::Value {
+        serde_json::to_value(cm).unwrap()
+    }
+
+    fn deserialize_from_json(json: serde_json::Value) -> ClusterMap {
+        serde_json::from_value(json.clone()).unwrap()
+    }
+
+    #[divan::bench(consts = SEEDS)]
+    fn serialize_proto<const S: u64>(b: Bencher) {
+        let gc = gen_cluster_map::<S>();
+        b.counter(gc.total_endpoints)
+            .bench(|| divan::black_box(serialize_to_protobuf(&gc.cm)));
+    }
+
+    #[divan::bench(consts = SEEDS)]
+    fn serialize_json<const S: u64>(b: Bencher) {
+        let gc = gen_cluster_map::<S>();
+        b.counter(gc.total_endpoints)
+            .bench(|| divan::black_box(serialize_to_json(&gc.cm)));
+    }
+
+    #[divan::bench(consts = SEEDS)]
+    fn deserialize_json<const S: u64>(b: Bencher) {
+        let gc = gen_cluster_map::<S>();
+        let json = serialize_to_json(&gc.cm);
+
+        b.with_inputs(|| json.clone())
+            .counter(gc.total_endpoints)
+            .bench_values(|json| divan::black_box(deserialize_from_json(json)));
+    }
+
+    #[divan::bench(consts = SEEDS)]
+    fn deserialize_proto<const S: u64>(b: Bencher) {
+        let gc = gen_cluster_map::<S>();
+        let pv = serialize_to_protobuf(&gc.cm);
+
+        b.with_inputs(|| pv.clone())
+            .counter(gc.total_endpoints)
+            .bench_values(|pv| divan::black_box(deserialize_from_protobuf(pv)));
+    }
+}
+
+const SEEDS: &[u64] = &[100, 200, 300, 400, 500];
+
+#[divan::bench_group(sample_count = 10)]
+mod ops {
+    use super::*;
+
+    fn compute_hash<const S: u64>(gc: &GenCluster) -> usize {
+        let mut total_endpoints = 0;
+
+        for kv in gc.cm.iter() {
+            for _ep in kv.value() {
+                total_endpoints += 1;
+            }
+        }
+
+        assert_eq!(total_endpoints, gc.total_endpoints);
+        total_endpoints
+    }
+
+    #[allow(clippy::eq_op)]
+    fn is_equal(gc: &GenCluster) -> usize {
+        assert_eq!(gc.cm, gc.cm);
+        gc.total_endpoints
+    }
+
+    #[divan::bench(consts = SEEDS)]
+    fn iterate<const S: u64>(b: Bencher) {
+        let cm = gen_cluster_map::<S>();
+
+        b.counter(cm.total_endpoints)
+            .bench_local(|| divan::black_box(compute_hash::<S>(&cm)));
+
+        drop(cm);
+    }
+
+    #[divan::bench(consts = SEEDS)]
+    fn iterate_par<const S: u64>(b: Bencher) {
+        let cm = gen_cluster_map::<S>();
+
+        b.counter(cm.total_endpoints)
+            .bench(|| divan::black_box(compute_hash::<S>(&cm)))
+    }
+
+    #[divan::bench(consts = SEEDS)]
+    fn partial_eq<const S: u64>(b: Bencher) {
+        let cm = gen_cluster_map::<S>();
+
+        b.counter(cm.total_endpoints)
+            .bench(|| divan::black_box(is_equal(&cm)))
+    }
+}
+
+fn main() {
+    divan::main();
+}
--- src/net/cluster.rs
+++ src/net/cluster.rs
@@ -15,7 +15,8 @@
  */
 
 use std::{
-    collections::BTreeSet,
+    collections::{hash_map::RandomState, BTreeSet},
+    fmt,
     sync::atomic::{AtomicU64, AtomicUsize, Ordering::Relaxed},
 };
 
@@ -28,7 +29,7 @@ use crate::net::endpoint::{Endpoint, Locality};
 const SUBSYSTEM: &str = "cluster";
 
 crate::include_proto!("quilkin.config.v1alpha1");
-pub(crate) use self::quilkin::config::v1alpha1 as proto;
+pub use self::quilkin::config::v1alpha1 as proto;
 
 pub(crate) fn active_clusters() -> &'static prometheus::IntGauge {
     static ACTIVE_CLUSTERS: Lazy<prometheus::IntGauge> = Lazy::new(|| {
@@ -59,8 +60,6 @@ pub(crate) fn active_endpoints() -> &'static prometheus::IntGauge {
 
     &ACTIVE_ENDPOINTS
 }
-
-use std::fmt;
 
 #[derive(Copy, Clone, PartialEq, Eq)]
 pub struct EndpointSetVersion(u64);
@@ -178,20 +177,44 @@ impl EndpointSet {
 }
 
 /// Represents a full snapshot of all clusters.
-#[derive(Default, Debug)]
-pub struct ClusterMap {
-    map: DashMap<Option<Locality>, EndpointSet>,
+pub struct ClusterMap<S = RandomState> {
+    map: DashMap<Option<Locality>, EndpointSet, S>,
     num_endpoints: AtomicUsize,
     version: AtomicU64,
 }
 
-type DashMapRef<'inner> = dashmap::mapref::one::Ref<'inner, Option<Locality>, EndpointSet>;
+type DashMapRef<'inner, S> = dashmap::mapref::one::Ref<'inner, Option<Locality>, EndpointSet, S>;
+type DashMapRefMut<'inner, S> =
+    dashmap::mapref::one::RefMut<'inner, Option<Locality>, EndpointSet, S>;
 
-impl ClusterMap {
+impl ClusterMap<RandomState> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
     pub fn new_default(cluster: BTreeSet<Endpoint>) -> Self {
         let this = Self::default();
         this.insert_default(cluster);
         this
+    }
+}
+
+impl<S> ClusterMap<S> {
+    #[inline]
+    pub fn version(&self) -> u64 {
+        self.version.load(Relaxed)
+    }
+}
+
+impl<S> ClusterMap<S>
+where
+    S: Default + std::hash::BuildHasher + Clone,
+{
+    pub fn benchmarking(capacity: usize, hasher: S) -> Self {
+        Self {
+            map: DashMap::with_capacity_and_hasher(capacity, hasher),
+            ..Self::default()
+        }
     }
 
     #[inline]
@@ -241,14 +264,20 @@ impl ClusterMap {
         self.map.is_empty()
     }
 
-    #[inline]
-    pub fn get(&self, key: &Option<Locality>) -> Option<DashMapRef> {
+    pub fn get(&self, key: &Option<Locality>) -> Option<DashMapRef<S>> {
         self.map.get(key)
     }
 
-    #[inline]
-    pub fn get_default(&self) -> Option<DashMapRef> {
+    pub fn get_mut(&self, key: &Option<Locality>) -> Option<DashMapRefMut<S>> {
+        self.map.get_mut(key)
+    }
+
+    pub fn get_default(&self) -> Option<DashMapRef<S>> {
         self.get(&None)
+    }
+
+    pub fn get_default_mut(&self) -> Option<DashMapRefMut<S>> {
+        self.get_mut(&None)
     }
 
     #[inline]
@@ -297,6 +326,18 @@ impl ClusterMap {
     }
 
     #[inline]
+    pub fn iter(&self) -> dashmap::iter::Iter<Option<Locality>, EndpointSet, S> {
+        self.map.iter()
+    }
+
+    pub fn entry(
+        &self,
+        key: Option<Locality>,
+    ) -> dashmap::mapref::entry::Entry<Option<Locality>, EndpointSet, S> {
+        self.map.entry(key)
+    }
+
+    #[inline]
     pub fn replace(&self, locality: Option<Locality>, endpoint: Endpoint) -> Option<Endpoint> {
         if let Some(mut set) = self.map.get_mut(&locality) {
             let replaced = set.endpoints.replace(endpoint);
@@ -312,11 +353,6 @@ impl ClusterMap {
             self.insert(locality, [endpoint].into());
             None
         }
-    }
-
-    #[inline]
-    pub fn iter(&self) -> dashmap::iter::Iter<Option<Locality>, EndpointSet> {
-        self.map.iter()
     }
 
     #[inline]
@@ -366,11 +402,6 @@ impl ClusterMap {
     }
 
     #[inline]
-    pub fn version(&self) -> u64 {
-        self.version.load(Relaxed)
-    }
-
-    #[inline]
     pub fn update_unlocated_endpoints(&self, locality: Locality) {
         if let Some((_, set)) = self.map.remove(&None) {
             self.version.fetch_add(1, Relaxed);
@@ -392,7 +423,7 @@ impl ClusterMap {
     }
 }
 
-impl crate::config::watch::Watchable for ClusterMap {
+impl<S> crate::config::watch::Watchable for ClusterMap<S> {
     #[inline]
     fn mark(&self) -> crate::config::watch::Marker {
         crate::config::watch::Marker::Version(self.version())
@@ -408,6 +439,31 @@ impl crate::config::watch::Watchable for ClusterMap {
     }
 }
 
+impl<S> fmt::Debug for ClusterMap<S>
+where
+    S: Default + std::hash::BuildHasher + Clone,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ClusterMap")
+            .field("map", &self.map)
+            .field("version", &self.version)
+            .finish_non_exhaustive()
+    }
+}
+
+impl<S> Default for ClusterMap<S>
+where
+    S: Default + std::hash::BuildHasher + Clone,
+{
+    fn default() -> Self {
+        Self {
+            map: <DashMap<Option<Locality>, EndpointSet, S>>::default(),
+            version: <_>::default(),
+            num_endpoints: <_>::default(),
+        }
+    }
+}
+
 impl Clone for ClusterMap {
     fn clone(&self) -> Self {
         let map = self.map.clone();
@@ -416,7 +472,10 @@ impl Clone for ClusterMap {
 }
 
 #[cfg(test)]
-impl PartialEq for ClusterMap {
+impl<S> PartialEq for ClusterMap<S>
+where
+    S: Default + std::hash::BuildHasher + Clone,
+{
     fn eq(&self, rhs: &Self) -> bool {
         for a in self.iter() {
             match rhs
@@ -510,7 +569,10 @@ impl Serialize for ClusterMap {
     }
 }
 
-impl From<ClusterMapDeser> for ClusterMap {
+impl<S> From<ClusterMapDeser> for ClusterMap<S>
+where
+    S: Default + std::hash::BuildHasher + Clone,
+{
     fn from(cmd: ClusterMapDeser) -> Self {
         let map = DashMap::from_iter(cmd.endpoints.into_iter().map(
             |EndpointWithLocality {
@@ -523,8 +585,11 @@ impl From<ClusterMapDeser> for ClusterMap {
     }
 }
 
-impl From<DashMap<Option<Locality>, EndpointSet>> for ClusterMap {
-    fn from(map: DashMap<Option<Locality>, EndpointSet>) -> Self {
+impl<S> From<DashMap<Option<Locality>, EndpointSet, S>> for ClusterMap<S>
+where
+    S: Default + std::hash::BuildHasher + Clone,
+{
+    fn from(map: DashMap<Option<Locality>, EndpointSet, S>) -> Self {
         let num_endpoints = AtomicUsize::new(map.iter().map(|kv| kv.value().len()).sum());
         Self {
             map,
@@ -570,11 +635,11 @@ mod tests {
 
     #[test]
     fn merge() {
-        let nl1 = Locality::region("nl-1");
-        let de1 = Locality::region("de-1");
+        let nl1 = Locality::with_region("nl-1");
+        let de1 = Locality::with_region("de-1");
 
         let mut endpoint = Endpoint::new((Ipv4Addr::LOCALHOST, 7777).into());
-        let cluster1 = ClusterMap::default();
+        let cluster1 = ClusterMap::new();
 
         cluster1.insert(Some(nl1.clone()), [endpoint.clone()].into());
         cluster1.insert(Some(de1.clone()), [endpoint.clone()].into());
