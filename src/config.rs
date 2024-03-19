9855b81d06689359ec8da2e9b492735d999d33af
Jake Shadle <jake.shadle@embark-studios.com>
2024-03-19 13:42:11 +0100

    Use simple fixed array for IcaoCode

--- src/config.rs
+++ src/config.rs
@@ -656,12 +656,19 @@ pub struct Datacenter {
     pub icao_code: IcaoCode,
 }
 
-#[derive(Clone, Debug, Hash, Eq, PartialEq, JsonSchema, Serialize, Deserialize)]
-pub struct IcaoCode(String);
+#[derive(Copy, Clone, Hash, Eq, PartialEq)]
+pub struct IcaoCode([u8; 4]);
+
+impl AsRef<str> for IcaoCode {
+    fn as_ref(&self) -> &str {
+        // SAFETY: We don't allow this to be constructed with an invalid utf-8 string
+        unsafe { std::str::from_utf8_unchecked(&self.0) }
+    }
+}
 
 impl Default for IcaoCode {
     fn default() -> Self {
-        Self("XXXX".to_owned())
+        Self([b'X', b'X', b'X', b'X'])
     }
 }
 
@@ -669,17 +676,91 @@ impl std::str::FromStr for IcaoCode {
     type Err = eyre::Error;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        if input.len() == 4 {
-            Ok(Self(input.to_owned()))
-        } else {
-            Err(eyre::eyre!("invalid ICAO code"))
+        const VALID_RANGE: std::ops::RangeInclusive<char> = 'A'..='Z';
+        let mut arr = [0; 4];
+        let mut i = 0;
+
+        for c in input.chars() {
+            eyre::ensure!(i < 4, "ICAO code is too long");
+            eyre::ensure!(
+                VALID_RANGE.contains(&c),
+                "ICAO code contained invalid character '{c}'"
+            );
+            arr[i] = c as u8;
+            i += 1;
         }
+
+        eyre::ensure!(i == 4, "ICAO code was not long enough");
+        Ok(Self(arr))
     }
 }
 
-impl std::fmt::Display for IcaoCode {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        self.0.fmt(f)
+use std::fmt;
+
+impl fmt::Display for IcaoCode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&self.as_ref())
+    }
+}
+
+impl fmt::Debug for IcaoCode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&self.as_ref())
+    }
+}
+
+impl Serialize for IcaoCode {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(self.as_ref())
+    }
+}
+
+impl<'de> Deserialize<'de> for IcaoCode {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct IcaoVisitor;
+
+        impl<'de> serde::de::Visitor<'de> for IcaoVisitor {
+            type Value = IcaoCode;
+            fn expecting(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                f.write_str("a 4-character, uppercase, alphabetical ASCII ICAO code")
+            }
+
+            fn visit_borrowed_str<E>(self, v: &'de str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                v.parse().map_err(serde::de::Error::custom)
+            }
+        }
+
+        deserializer.deserialize_str(IcaoVisitor)
+    }
+}
+
+impl schemars::JsonSchema for IcaoCode {
+    fn schema_name() -> String {
+        "IcaoCode".into()
+    }
+
+    fn is_referenceable() -> bool {
+        false
+    }
+
+    fn json_schema(gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+        let mut schema = gen.subschema_for::<String>();
+        if let schemars::schema::Schema::Object(schema_object) = &mut schema {
+            if schema_object.has_type(schemars::schema::InstanceType::String) {
+                let validation = schema_object.string();
+                validation.pattern = Some(r"^[A-Z]{4}$".to_string());
+            }
+        }
+        schema
     }
 }
 
