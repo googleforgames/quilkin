4c6701da843ed52a27cfdc2eb88ac22440537774
markmandel <markmandel@google.com>
2024-05-02 19:21:05 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@df7b5f26ad13221440d1064e9f1d7343b2ed0089 🚀

---
+++ main/api/trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"quilkin":[["impl ValueEnum for <a class=\"enum\" href=\"quilkin/config/enum.AddrKind.html\" title=\"enum quilkin::config::AddrKind\">AddrKind</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
