b4bd4cfa25430d609d557e9aa816a45663f8b8c3
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2024-05-20 11:33:52 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@2e150019f51c88c84e3e29d7240ee0843b8356a8 🚀

--- main/api/quilkin/net/xds/sidebar-items.js
+++ main/api/quilkin/net/xds/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AwaitingAck","ClientVersions","Resource","ResourceType"],"struct":["Client"],"type":["AdsClient"]};
+window.SIDEBAR_ITEMS = {"enum":["AwaitingAck","ClientVersions","Resource","ResourceType"],"mod":["server"],"struct":["Client"],"type":["AdsClient"]};
