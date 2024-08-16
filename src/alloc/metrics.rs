186a734e43199aec7f67609fa3d1d0ed84146065
Jake Shadle <jake.shadle@embark-studios.com>
2024-08-16 10:50:48 +0200

    Headers

--- src/alloc/metrics.rs
+++ src/alloc/metrics.rs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /// Spawns a task to periodically update the heap stat metrics from our tracking allocator
 #[cfg(feature = "heap-stats")]
 pub fn spawn_heap_stats_updates(period: std::time::Duration, mut srx: crate::ShutdownRx) {
--- src/alloc/tracking.rs
+++ src/alloc/tracking.rs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 use std::alloc::Layout;
 
 cfg_if::cfg_if! {
@@ -146,10 +162,6 @@ impl PackedAtomicCounter {
             size: ((packed >> 30) & ((1 << 34) - 1)),
         }
     }
-
-    // pub fn reset(&self) {
-    //     self.0.store(0, Relaxed);
-    // }
 }
 
 struct Bucket {
@@ -253,13 +265,6 @@ impl StatsAccumulator {
 
         stats
     }
-
-    // pub fn reset(&self) {
-    //     for bucket in &self.buckets {
-    //         bucket.cumul_alloc.reset();
-    //         bucket.cumul_free.reset();
-    //     }
-    // }
 
     #[inline(always)]
     fn bucket(&self, ptr: usize) -> &Bucket {
