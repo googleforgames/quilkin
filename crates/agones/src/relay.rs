10e580d187f160187f55ec69e7e7e119b4591516
Jake Shadle <jake.shadle@embark-studios.com>
2024-09-05 10:32:50 +0200

    ...

--- crates/agones/src/relay.rs
+++ crates/agones/src/relay.rs
@@ -51,7 +51,7 @@ mod tests {
         TOKEN_KEY,
     };
 
-    const SLOW: Duration = Duration::from_secs(60);
+    const SLOW: Duration = Duration::from_secs(120);
 
     #[tokio::test]
     #[serial]
