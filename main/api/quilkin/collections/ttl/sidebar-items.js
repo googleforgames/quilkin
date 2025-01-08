b42d46b788029855deb0e8bf3f4229a5fdd236b3
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2025-01-08 15:13:33 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@1ace05d36f757aebd6c1c676a80d4689be324cae 🚀

---
+++ main/api/quilkin/collections/ttl/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Entry","TryResult"],"struct":["OccupiedEntry","TtlMap","VacantEntry","Value"]};
--- main/api/trait.impl/core/ops/deref/trait.DerefMut.js
+++ main/api/trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["quilkin",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"quilkin/pool/struct.PoolBuffer.html\" title=\"struct quilkin::pool::PoolBuffer\">PoolBuffer</a>"]]]]);
+    var implementors = Object.fromEntries([["quilkin",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"quilkin/collections/pool/struct.PoolBuffer.html\" title=\"struct quilkin::collections::pool::PoolBuffer\">PoolBuffer</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[301]}
+//{"start":57,"fragment_lengths":[326]}
