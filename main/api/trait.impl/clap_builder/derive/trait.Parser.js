ce526f5c2b6223bbe1996202cc0a151ca6eda388
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2025-01-22 18:32:43 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@0e4b949eab9a7b417a8ed9bf1dc6cc29e59c2dc4 🚀

--- main/api/trait.impl/clap_builder/derive/trait.Parser.js
+++ main/api/trait.impl/clap_builder/derive/trait.Parser.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["quilkin",[["impl Parser for <a class=\"struct\" href=\"quilkin/cli/struct.AdminCli.html\" title=\"struct quilkin::cli::AdminCli\">AdminCli</a>"],["impl Parser for <a class=\"struct\" href=\"quilkin/cli/struct.LocalityCli.html\" title=\"struct quilkin::cli::LocalityCli\">LocalityCli</a>"],["impl Parser for <a class=\"struct\" href=\"quilkin/struct.Cli.html\" title=\"struct quilkin::Cli\">Cli</a>"]]]]);
+    var implementors = Object.fromEntries([["quilkin",[["impl Parser for <a class=\"struct\" href=\"quilkin/cli/struct.AdminCli.html\" title=\"struct quilkin::cli::AdminCli\">AdminCli</a>"],["impl Parser for <a class=\"struct\" href=\"quilkin/cli/struct.LocalityCli.html\" title=\"struct quilkin::cli::LocalityCli\">LocalityCli</a>"],["impl Parser for <a class=\"struct\" href=\"quilkin/cli/struct.Service.html\" title=\"struct quilkin::cli::Service\">Service</a>"],["impl Parser for <a class=\"struct\" href=\"quilkin/struct.Cli.html\" title=\"struct quilkin::Cli\">Cli</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[403]}
+//{"start":57,"fragment_lengths":[535]}
