fd33489b080c10a7b07a74576d3075bf6c23efaf
Jake Shadle <jake.shadle@embark-studios.com>
2025-01-24 14:04:52 +0100

    Add a bash script that uses veth interfaces to test XDP

---
+++ .ci/xdp/veth-integ-test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -e
+
+source="${BASH_SOURCE[0]}"
+
+cleanup() {
+    echo "Cleaning up"
+    ip netns del cs || true
+    ip netns del proxy || true
+
+    pkill fortio || true
+    pkill quilkin || true
+}
+
+trap cleanup EXIT
+
+ip netns del cs || true
+ip netns del proxy || true
+
+echo "::notice file=$source,line=$LINENO::Creating network namespaces"
+ip netns add cs
+ip netns add proxy
+
+echo "::notice file=$source,line=$LINENO::Adding client <-> proxy <-> server links"
+ip link add veth-cs type veth peer name veth-proxy
+
+ip link set veth-cs netns cs
+ip link set veth-proxy netns proxy
+
+echo "::notice file=$source,line=$LINENO::Adding IPs"
+ip -n cs addr add 10.0.0.1/24 dev veth-cs
+ip -n proxy addr add 10.0.0.2/24 dev veth-proxy
+
+echo "::notice file=$source,line=$LINENO::Creating network namespaces"
+ip -n cs link set veth-cs up
+ip -n proxy link set veth-proxy up
+
+ip netns exec cs fortio udp-echo&
+ip netns exec proxy ./target/debug/quilkin proxy --to 10.0.0.1:8078 --publish.udp.xdp&
+
+echo "::notice file=$source,line=$LINENO::Launching client"
+ip netns exec cs fortio load -n 10 udp://10.0.0.2:7777 2> ./target/logs.txt
+logs=$(cat ./target/logs.txt)
+echo "$logs"
+
+regex="Total Bytes sent: ([0-9]+), received: ([0-9]+)"
+
+if [[ $logs =~ $regex ]]; then
+  send=${BASH_REMATCH[1]}
+  recv=${BASH_REMATCH[2]}
+  # We could be more strict here and require they are exactly equal, but I can't
+  # even consistently get that on my local machine so I doubt CI will fair better
+  if [[ $recv -ne "0" ]]; then
+    echo "::notice file=$source,line=$LINENO::Successfully sent ${send}B and received ${recv}B"
+    exit 0
+  fi
+
+  echo "::error file=$source,line=$LINENO::sent ${send}B but only received ${recv}B"
+  exit 1
+fi
+
+echo "::error file=$source,line=$LINENO::Failed to find expected log line from UDP client"
+exit 2
