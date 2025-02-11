adfbc74eafd6881b42f6dfbfafca20ffdcc1af77
Jake-Shadle <jake.shadle@embark-studios.com>
2025-02-11 14:13:18 +0000

    Deploying to gh-pages from @ googleforgames/quilkin@df4d0d910a71a9ab4fcbdb71155fd469fdb352e9 🚀

--- main/api/quilkin_xdp/sidebar-items.js
+++ main/api/quilkin_xdp/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["BindError","LoadError"],"fn":["get_default_nic"],"struct":["EbpfProgram","NicIndex","XdpWorker"]};
+window.SIDEBAR_ITEMS = {"enum":["BindError","LoadError"],"struct":["EbpfProgram","NicIndex","XdpWorker"]};
