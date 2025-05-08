5b53ac00ff8be8a1b08f05caf6a9003523b4881c
Jake Shadle <jake.shadle@embark-studios.com>
2025-05-08 09:52:00 +0200

    Add GRPC flag

--- src/service.rs
+++ src/service.rs
@@ -89,6 +89,13 @@ pub struct Service {
         default_value_t = 7800
     )]
     xds_port: u16,
+    /// Whether to serve xDS and/or mDS requests.
+    #[arg(
+        long = "service.grpc",
+        env = "QUILKIN_SERVICE_GRPC",
+        default_value_t = false
+    )]
+    grpc_enabled: bool,
     /// A PEM encoded certificate, if supplied, applies to the mds and xds service(s)
     #[clap(
         long = "service.tls.cert",
@@ -140,6 +147,7 @@ impl Default for Service {
             udp_workers: std::num::NonZeroUsize::new(num_cpus::get()).unwrap(),
             xds_enabled: <_>::default(),
             xds_port: 7800,
+            grpc_enabled: false,
             xdp: <_>::default(),
             tls_cert: None,
             tls_key: None,
@@ -215,6 +223,13 @@ impl Service {
         self
     }
 
+    pub fn grpc(mut self) -> Self {
+        self.mds_enabled = true;
+        self.xds_enabled = true;
+        self.grpc_enabled = true;
+        self
+    }
+
     pub fn xdp(mut self, xdp_opts: XdpOptions) -> Self {
         self.xdp = xdp_opts;
         self
@@ -227,6 +242,7 @@ impl Service {
             || self.phoenix_enabled
             || self.xds_enabled
             || self.mds_enabled
+            || self.grpc_enabled
     }
 
     pub fn build_config(&self, icao_code: IcaoCode) -> eyre::Result<Config> {
@@ -441,7 +457,7 @@ impl Service {
         &self,
         config: &Arc<Config>,
     ) -> crate::Result<impl Future<Output = crate::Result<()>> + use<>> {
-        if !self.xds_enabled {
+        if !self.xds_enabled && !self.grpc_enabled {
             return Ok(either::Left(std::future::pending()));
         }
 
@@ -467,7 +483,7 @@ impl Service {
         &self,
         config: &Arc<Config>,
     ) -> crate::Result<impl Future<Output = crate::Result<()>> + use<>> {
-        if !self.mds_enabled {
+        if !self.mds_enabled && !self.grpc_enabled {
             return Ok(either::Left(std::future::pending()));
         }
 
