9a69da7412f9ff5fec6058dfdcadbeaafed7bd89
Gustaf Lindstedt <gustaflindstedt@protonmail.com>
2025-06-24 11:01:02 +0200

    TEST put qcmp on own thread to investigate issue

--- src/service.rs
+++ src/service.rs
@@ -443,12 +443,7 @@ impl Service {
         tracing::info!(port=%self.qcmp_port, "starting qcmp service");
         let qcmp = crate::net::raw_socket_with_reuse(self.qcmp_port)?;
 
-        let join = crate::codec::qcmp::spawn(qcmp, qcmp_port.subscribe(), shutdown.shutdown_rx())?;
-        let finished = shutdown.push("qcmp");
-        tokio::spawn(async move {
-            drop(join.await);
-            drop(finished.send(Ok(())));
-        });
+        crate::codec::qcmp::spawn(qcmp, qcmp_port.subscribe(), shutdown)?;
 
         Ok(())
     }
